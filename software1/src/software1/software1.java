402b780347d8e818b394c84e3864f7ab2dc3b30d
tanakayuki <tanakayuki@192.168.0.4>
2020-05-15 16:47:20 +0900

    Yuki

---
+++ software1/src/software1/software1.java
@@ -0,0 +1,13 @@
+package software1;
+
+public class software1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		System.out.println("ポテト");
+		System.out.println("ホクホクしてて美味しいから。");
+
+	}
+
+}
