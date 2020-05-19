4eeec3eec5829dd1f4638326564b06451819a92c
lika <lika@172.20.10.2>
2020-05-19 22:24:20 +0900

    りか

--- software1/src/software1/software1.java
+++ software1/src/software1/software1.java
@@ -4,9 +4,10 @@ public class software1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 		System.out.println("ポテト");
 		System.out.println("ホクホクしてて美味しいから。");
+		System.out.println("しなしなも美味しいよ。byりか");
 
 	}
 
